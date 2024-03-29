5da557183b78eb0b0c04f5034a6617b9b551ec1f
Bruce Aldridge <bruce.aldridge@gmail.com>
2024-03-29 17:05:59 +1300

    remove copy paste

--- src/class-area-lookup.php
+++ src/class-area-lookup.php
@@ -12,9 +12,7 @@ class area_lookup {
 	 * @return string
 	 */
 	public function get_area( $lat, $lon ) {
-		// make a get request to get the area for the location from openmeteo.
-		// here is a sample request
-		// https://openmeteo.org/api/0.1/area?lat=51.5074&lon=0.1278
+		// make a get request to get the area for the location from nominatim.
 		$response = wp_remote_get( "https://nominatim.openstreetmap.org/reverse?format=json&lat=$lat&lon=$lon&zoom=14&addressdetails=1" );
 
 		// the response is a json object with the area data, we can return the output directly
