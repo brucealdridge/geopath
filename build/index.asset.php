0c65d223821a13a998a55f5c907499d4ab5c7ec3
Bruce Aldridge <bruce.aldridge@gmail.com>
2024-04-02 17:12:06 +1300

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4153305a47116584260e');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e7c5f5f3737d2e7c0fbb');
